8cb6b0c560fe6ddd1d4e940de1a831db47fdb16b
Suraj-codes1410 <samantasuraj1410@gmail.com>
2026-06-16 16:22:17 +0530

    updated the index section

---
+++ lib/commands/index.ts
@@ -0,0 +1,57 @@
+import { registry } from './registry';
+import { helpCommand } from './help';
+import { clearCommand } from './clear';
+import { versionCommand } from './version';
+import { aboutCommand } from './about';
+import { skillsCommand } from './skills';
+import { projectsCommand } from './projects';
+import { experienceCommand } from './experience';
+import { educationCommand } from './education';
+import { achievementsCommand } from './achievements';
+import { contactCommand } from './contact';
+import { resumeCommand } from './resume';
+import { homeCommand } from './home';
+import { recruiterCommand } from './recruiter';
+import { openCommand } from './open';
+import { projectCommand } from './project';
+
+// Register core commands on module load
+registry.register(helpCommand);
+registry.register(clearCommand);
+registry.register(versionCommand);
+registry.register(aboutCommand);
+registry.register(skillsCommand);
+registry.register(projectsCommand);
+registry.register(experienceCommand);
+registry.register(educationCommand);
+registry.register(achievementsCommand);
+registry.register(contactCommand);
+registry.register(resumeCommand);
+registry.register(homeCommand);
+registry.register(recruiterCommand);
+registry.register(openCommand);
+registry.register(projectCommand);
+
+export * from './types';
+export * from './registry';
+export * from './parser';
+export * from './handler';
+export { 
+  helpCommand, 
+  clearCommand, 
+  versionCommand, 
+  aboutCommand, 
+  skillsCommand, 
+  projectsCommand, 
+  experienceCommand, 
+  educationCommand, 
+  achievementsCommand, 
+  contactCommand, 
+  resumeCommand,
+  homeCommand,
+  recruiterCommand,
+  openCommand,
+  projectCommand
+};
+
+
