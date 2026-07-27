76d53b5e7d4fcc2a7a858c0a92d0ad9312a1fe56
Suraj-codes1410 <samantasuraj1410@gmail.com>
2026-07-28 00:52:47 +0530

    fix(layout): swap ShellProvider and SharedLayout nesting order to resolve useShell context runtime errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,16 +36,16 @@ export default function RootLayout({
       className={`${inter.variable} ${jetbrainsMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-bg-primary text-text-primary">
-        <SharedLayout>
-          <ShellProvider>
+        <ShellProvider>
+          <SharedLayout>
             <BootSequence />
             <Navbar />
             <main className="flex-grow flex flex-col">
               {children}
             </main>
             <Footer />
-          </ShellProvider>
-        </SharedLayout>
+          </SharedLayout>
+        </ShellProvider>
       </body>
     </html>
   );
